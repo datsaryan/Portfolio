7dcab5fb5a33f25701f5c676d64da3864c60a75b
datsaryan <aryansobdh@gmail.com>
2026-08-29 20:54:27 +0530

    final portfolio

--- Aryan Portfolio/portfolio-backend/src/main/java/com/aryan/portfolio/repository/ContactMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aryan.portfolio.repository;
-
-import com.aryan.portfolio.model.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
-}
